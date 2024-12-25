54945b8c4e4f3efd306337ab94c688677685cac3
Tejas Tirthapura <tirthapuratejas@gmail.com>
2024-12-24 23:57:52 -0800

    added functionality for radio buttons

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let paint = (box) => box.style.backgroundColor = "white";
+
 function makeGrid(size) {
     const grid = document.querySelector(".container");
     grid.innerHTML = "";
@@ -11,10 +13,7 @@ function makeGrid(size) {
             box.classList.add("box");
             box.style.width = 500 / size + "px";
             box.style.height = 500 / size + "px";
-            box.addEventListener("mouseenter", function paint() {
-                box.style.backgroundColor = "white";
-            }
-            );
+            box.addEventListener("mouseenter", () => paint(box));
             row.appendChild(box);
         }
 
@@ -22,6 +21,39 @@ function makeGrid(size) {
     }
 }
 
+document.querySelector("#white").addEventListener("change", () => paint = (box) => {
+    box.style.backgroundColor = "white";
+    box.style.opacity = 1;
+})
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+document.querySelector("#rainbow").addEventListener("change", () => paint = (box) => {
+    box.style.backgroundColor = getRandomColor();
+    box.style.opacity = 1;
+})
+
+document.querySelector("#shading").addEventListener("change", () => paint = (box) => {
+    box.style.opacity -= 0.1;
+})
+
+document.querySelector("#eraser").addEventListener("change", () => paint = (box) => {
+    box.style.backgroundColor = "black";
+    box.style.opacity = 1;
+})
+
+
+
+
 const newSize = document.querySelector(".resize");
+makeGrid(parseInt(newSize.value));
 const resetBtn = document.querySelector(".reset");
 resetBtn.addEventListener("click",() => makeGrid(parseInt(newSize.value)));
+console.log("success");
